c44b8ee223d4073089dbde1c6147a097069738c3
Fabián Santander <hdmsantander@gmail.com>
2021-09-01 09:28:05 -0500

    Change type to avoid mapping errors
    Sometimes the pet shop API will return a value outside of the int range.

--- query-microservice/src/main/java/mx/hdmsantander/opsdemo/query/model/Category.java
+++ query-microservice/src/main/java/mx/hdmsantander/opsdemo/query/model/Category.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Category {
 
-	private Integer id;
+	private String id;
 
 	private String name;
 
--- query-microservice/src/main/java/mx/hdmsantander/opsdemo/query/model/Tag.java
+++ query-microservice/src/main/java/mx/hdmsantander/opsdemo/query/model/Tag.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Tag {
 
-	private Integer id;
+	private String id;
 
 	private String name;
 
