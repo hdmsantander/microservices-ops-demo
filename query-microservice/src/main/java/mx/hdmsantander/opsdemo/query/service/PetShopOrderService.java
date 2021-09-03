25a421493c98663126e106e043521e2fca321a45
Fabián Santander <hdmsantander@gmail.com>
2021-09-03 17:39:56 -0500

    Fix gauge metric reporting

--- query-microservice/src/main/java/mx/hdmsantander/opsdemo/query/service/PetShopOrderService.java
+++ query-microservice/src/main/java/mx/hdmsantander/opsdemo/query/service/PetShopOrderService.java
@@ -1,12 +1,13 @@
 package mx.hdmsantander.opsdemo.query.service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.google.common.util.concurrent.AtomicDouble;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import mx.hdmsantander.opsdemo.query.data.PetShopOrderRepository;
@@ -20,13 +21,19 @@ public class PetShopOrderService {
 
 	@Autowired
 	private MeterRegistry meterRegistry;
-
+	
+	private AtomicDouble orderCount = new AtomicDouble();
+	
 	public List<PetShopOrder> getAllOrders() {
 
 		List<PetShopOrder> orders = StreamSupport.stream(petShopOrderRepository.findAll().spliterator(), false)
 				.collect(Collectors.toList());
-
-		return meterRegistry.gauge("orders.size", Collections.emptyList(), orders, List::size);
+		
+		orderCount.set(orders.size());
+		
+		meterRegistry.gauge("orders.size", orderCount);
+		
+		return orders;
 
 	}
 
